e948b78e2584c62ac6beac35520f016d2250c37f
SchutzenderZeus <maximilian.jatzlau@schiller-og.de>
2023-09-27 09:01:20 +0200

    a

--- src/Spiel.java
+++ src/Spiel.java
@@ -3,15 +3,27 @@ import java.util.LinkedList;
 
 public class Spiel extends Karte{
 
-    static Karte aktuelleKarte = new Karte();
+    public static Karte aktuelleKarte = new Karte();
     public static int gezogeneKarten = 0;
     public static int entfernteKarten = 0;
+    public static int position = 0;
 
-LinkedList<Karte> satz = new LinkedList<Karte>();
+
+    static LinkedList<Karte> geKarten = new LinkedList<Karte>();
+static LinkedList<Karte> satz = new LinkedList<Karte>();
+
 
     public static void main(String[] args) {
 
-        zeigeAktuelleKarte();
+
+        for (int i=0;i<=10;i++){
+            karteAnlegen();
+            zeigeAktuelleKarte();
+            //System.out.println(geKarten.get(i).getWort());
+            karteZiehen();
+        }
+        getGesamtPunktzahl();
+        infoAusgeben();
 
 
     }
@@ -24,12 +36,51 @@ public Spiel(){
     public static void karteZiehen(){
         if (gezogeneKarten < 10){
             gezogeneKarten = gezogeneKarten+1;
+            geKarten.add(aktuelleKarte);
+            aktuelleKarte = new Karte();
+        }
+        else{
+            System.out.println("Es wurden bereits 10 Karten gezogen");
         }
     }
 
     public static void zeigeAktuelleKarte(){
-        System.out.println(aktuelleKarte);
+        System.out.println(aktuelleKarte.getWort());
+        System.out.println(aktuelleKarte.getWert());
     }
+
+    public static void karteAnlegen(){
+        satz.add(position,aktuelleKarte);
+        position++;
+    }
+
+    public static void entferneKarte(){
+        satz.remove(position);
+    }
+
+    public static void infoAusgeben(){
+        for (int i=0;i<=satz.size();i++){
+            karteZiehen();
+            System.out.print(satz.get(i).getWort()+" ");
+        }
+
+    }
+    public static void getGesamtPunktzahl(){
+        int ergebnis= 0;
+        int aktuell=0;
+
+        for (int i=0;i<=10;i++){
+            karteZiehen();
+            aktuell = satz.get(i).getWert();
+            ergebnis = ergebnis+aktuell;
+        }
+        System.out.println("Aktueller Punktestand: "+ergebnis);
+    }
+
+    public static void positionsBestimmung(){
+    position = 1;
+    }
+
 
     @Override
     public String getWort() {
